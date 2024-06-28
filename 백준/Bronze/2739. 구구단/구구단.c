b8687b46214bed795892449277dd3ecce61799e0
rivertogg <167750764+stoogg@users.noreply.github.com>
2024-06-28 17:43:53 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 0;
+	scanf("%d", &n);
+	for (int i = 1; i <= 9; i++)
+	{
+		printf("%d * %d = %d\n", n, i, n * i);
+	}
+	return 0;
+}
