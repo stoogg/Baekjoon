23518b3f6f7f180a0e286109d296f6016a41d5b6
rivertogg <167750764+stoogg@users.noreply.github.com>
2024-07-04 20:13:18 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 288 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int n, a, b;
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d%d", &a, &b);
+		printf("%d\n", a + b);
+	}
+	
+	return 0;
+
+}
