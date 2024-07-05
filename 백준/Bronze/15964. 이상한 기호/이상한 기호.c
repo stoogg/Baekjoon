747c6c216b83823e2f8e57a898b0b341283e0e49
rivertogg <167750764+stoogg@users.noreply.github.com>
2024-07-05 20:12:35 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.c
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.c
@@ -1,21 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 #include <string.h>
-int AA (int a, int b)
-{
-    return a + b;
-}
 
-int BB(int a, int b)
-{
-    return a- b;
-}
 
 int main() // A＠B = (A+B)×(A-B)
 {
-    int A, B;
-    scanf("%d%d", &A, &B);
-   
-    
-    printf("%d", AA(A, B) *BB(A, B));
+    long long A, B;
+    scanf("%ld%ld", &A, &B);
+    printf("%ld", (A+B)*(A-B));
 }
